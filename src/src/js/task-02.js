5dee0cf61945dd51734c51a49d4fa5fdc4fc0c9a
CelebanPatrycja <114691633+CelebanPatrycja@users.noreply.github.com>
2023-03-07 18:58:53 +0100

    Update hw6

---
+++ src/src/js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const tab = document.querySelector("#ingredients");
+const list = [];
+
+//dodac li do nowej tablicy na pozycji x
+//pobrac i dodac tresc do li z indeksem x
+//dodac klase “item” do li z indeksem x
+
+for (let i = 0; i < ingredients.length; i++) {
+  list[i] = document.createElement("li");
+  list[i].textContent = ingredients[i];
+  list[i].classList.add("item");
+  list.push(list[i]);
+}
+
+tab.append(...list);
